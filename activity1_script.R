f669a20d0e5a1dea3dac6c84d8aa14e1854a4273
unknown <rarioux@hamilton.edu>
2020-09-02 22:28:59 -0400

    added some math for fun

--- activity1_script.R
+++ activity1_script.R
@@ -1,2 +1,23 @@
 #Activity 1
 print("hello R world")
+
+choose(10,3)
+#120
+
+47-5
+#42
+
+factorial(10)
+#3628800
+
+A <- 23129482094
+B <- 23897325932
+C <- 23783158734
+
+(A + B) / C
+#1.977
+
+A/B - C
+#-23783158733
+
+print("Goodbye R world... for now")
